d3acd0bd360f26695762a225a522eb4db747e6ba
kauana-sants <124754922+kauana-sants@users.noreply.github.com>
2025-06-09 21:15:40 -0300

    navbar
    criação da sidebar, estrutura principal e um pouco de estilo

--- genesys/src/Components/Navbar.jsx
+++ genesys/src/Components/Navbar.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom"
+import "./Navbar.scss"
+import { Outlet } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
+        {/* <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link> */}
+      <div className="barraLateralMenu">
+        <button><img src="" alt="Conversas" /></button>
+        <button><img src="" alt="configurações" /></button>
+        <button><img src="" alt="perfil" /></button>
+      </div>
+
+      <div className="LogOff">
+        <button><img src="" alt="logoff" /></button>
+      </div>
+          
     </nav>
   )
 }
